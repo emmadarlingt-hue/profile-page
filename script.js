f59833b7c97920df11578c1d9b39e0114f1ac32f
Emma Darling <emmadarlingt@gmail.com>
2026-05-26 13:29:10 +0100

    Add files via upload
    Add profile page files

---
+++ script.js
@@ -0,0 +1,6 @@
+ function showMessage() {
+  document.getElementById("message").innerHTML =
+    "Thanks for your interest, let's talk.";
+  document.getElementById("hireBtn").style.backgroundColor = "#4ecca3";
+  document.getElementById("hireBtn").style.color = "#1a1a2e";
+}
